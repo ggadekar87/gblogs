165d0c8a61858c4c94ecbea6cd4f635a0d0f77f2
ganesh.balasaheb <ganesh.balasaheb@maqta.ae>
2025-01-15 16:02:53 +0400

    Css changes

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -7,23 +7,29 @@ const Home = () => {
       <div className="container">
         <div>
           <div className="card">
-            <img src={CVImage} alt="Avatar" className="card-img" />
+            {/* <img src={CVImage} alt="Avatar" className="card-img" /> */}
+            <div className="DivUpperBorder">
+              Online Resume
+            </div>
             <div className="card-container">
-              <h4> <a href="https://mycv.pics/" rel="noreferrer" target="_blank"> View online cv</a> </h4>
+              <h4> <a href="https://mycv.pics/" className="linkCss" rel="noreferrer" target="_blank"> View online cv</a> </h4>
               <p>Are you looking to establish a strong online presence? Look no further! Our expert team specializes in creating stunning, user-friendly cv tailored to your unique needs. Whether you're an individual looking to showcase your portfolio, we have the perfect solution for you.</p>
             </div>
           </div>
         </div>
         <div>
           <div className="card">
+            <div className="DivUpperBorder">
+              Reduxt Toolkit
+            </div>
             <div className="card-container">
-              <h4>REACT REDUX TOOLKIT </h4>
+              <h4>  <NavLink className="linkCss"
+                to="redux-toolkit"
+              >
+                Redux Toolkit
+              </NavLink> </h4>
               <p>
-                <NavLink
-                  to="redux-toolkit"
-                >
-                  Learn react redux - toolkit
-                </NavLink>
+                Redux toolkit is used to manage redux store operation.
               </p>
             </div>
           </div>
